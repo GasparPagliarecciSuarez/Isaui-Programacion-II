f5256cdc8e262d03d09079d8b12f7c139fac4307
GasparPS <gaspar.suarez.pagliarecci@gmail.com>
2024-04-30 20:44:11 -0300

    Se modifico el texto indicativo

--- Conversor de Farenheit  a Celsius actualizado 2.0.py
+++ Conversor de Farenheit  a Celsius actualizado 2.0.py
@@ -1,15 +1,15 @@
 while True:
-    valor = int(input("Seleccione a que medida de temperatura desea convertir: 1) Farenheit, 2) celcius: "))
+    valor = int(input("Seleccione a que medida de temperatura desea convertir ingresando el número indicado: 1) Farenheit, 2) Celcius: "))
     #valor = input()
     if valor == 2:
         Grados = float(input("Por favor ingrese la temperatura en grados farenheit: "))
         conversion = (Grados - 32) * 5 / 9 
-        print ("Los grados celsius son:" + str (round(conversion,1)))
+        print ("Los grados Celsius son: " + str (round(conversion,1)))
         break
     if valor ==1:
         Grados = float(input("Por favor ingrese la temperatura en grados Celcius: "))
         conversion = (Grados * 1.8) + 32 
-        print ("Los grados Farenheit son:" + str (round(conversion,1)))
+        print ("Los grados Farenheit son: " + str (round(conversion,1)))
         break
     if valor !=1 and valor != 2:
         print ("Por favor Ingrese un número correcto para continuar con la conversión")
